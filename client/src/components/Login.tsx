11fee3fc3d8da459f679c6a6a68fc46429a05fd0
Danil Melkozerov <danmelk.dev@gmail.com>
2022-12-11 02:37:32 +0400

    feat: new login form

---
+++ client/src/components/Login.tsx
@@ -0,0 +1,10 @@
+export const Login = () => {
+    return (
+        <div className="container">
+            <form>
+                <input type="text" className="auth-field" id="" />
+                <button type="submit" className="submit-button">Get in.</button>
+            </form>
+        </div>
+    )   
+}
